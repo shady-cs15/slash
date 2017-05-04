706a6c17448cd0ff26ebf5d8d107fa2a3730dbe3
Satyaki Chakraborty <satyaki.cs15@gmail.com>
2017-05-05 00:22:18 +0530

    work with batch_size > 1

--- src/dev-script.py
+++ src/dev-script.py
@@ -14,22 +14,28 @@ data = [np.load('../data/'+ data_file)	for data_file in data_files]
 inputs = []
 labels = []
 
+'''
 for i in range(len(data)):
-	data_x = (data[i] )/ 3.75
+	data_x = (data[i] - 7.5) / 3.75
 	data_y = np.eye(16)[data[i]]
 	inputs.append(data_x.reshape(1, data_x.shape[0], 1))
 	labels.append(data_y.reshape(1, data_y.shape[0], 16))
+'''
+inputs = [np.load('dummyX.npy')]
+labels = [np.load('dummyY.npy')]
+masks = [np.load('dummyM.npy')]
 
-batch_size = 1
+batch_size = 64
 global_context_size = 100
 bptt_steps = 2
 n_epochs = 100
 clip_iter = 20
 input = tf.placeholder(tf.float32, [batch_size, global_context_size*bptt_steps+global_context_size-1, 1])
-label = tf.placeholder(tf.uint8, [batch_size, global_context_size*bptt_steps, 16])
-t_model = model.sample_rnn(input, label, is_training=False)
+label = tf.placeholder(tf.float32, [batch_size, global_context_size*bptt_steps, 16])
+mask = tf.placeholder(tf.float32, [batch_size, global_context_size*bptt_steps, 16])
+t_model = model.sample_rnn(input, label, mask, batch_size=batch_size, is_training=False)
 
-optimizer = tf.train.AdadeltaOptimizer(0.01)
+optimizer = tf.train.AdamOptimizer(0.01)
 global_step = tf.Variable(0)
 gradients, v = zip(*optimizer.compute_gradients(t_model.loss))
 gradients, _ = tf.clip_by_global_norm(gradients, 1.0)
@@ -40,6 +46,9 @@ if not os.path.exists('./params'):	os.makedirs('./params')
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
+	if os.path.exists('./params/last_model.ckpt.meta'):
+		saver.restore(sess, './params/last_model.ckpt')
+		print 'model restored from last checkpoint..'
 	z_state = (t_model.initial_state[0].eval(), t_model.initial_state[1].eval())
 
 	for ep in range(n_epochs):
@@ -56,12 +65,17 @@ with tf.Session() as sess:
 					end_ptr = (j+1)*global_context_size*bptt_steps + global_context_size - 1
 					bptt_batch_x = current_clip[:, start_ptr:end_ptr, :]
 					bptt_batch_y = labels[i][:, start_ptr+global_context_size:end_ptr+1, :]
-					bptt_batch_loss, acc, np_state, op, out, p1, p2 = sess.run([t_model.loss, t_model.mean_acc, t_model.final_state, optimizer, t_model.outputs, t_model.o1, t_model.o2], feed_dict={input:bptt_batch_x, label:bptt_batch_y,
-															t_model.initial_state[0]:np_state[0], t_model.initial_state[1]:np_state[1]})
+					bptt_batch_m = masks[i][:, start_ptr+global_context_size:end_ptr+1, :]
+					bptt_batch_loss, acc, np_state, op, out = \
+						sess.run([t_model.loss, t_model.mean_acc, t_model.final_state, optimizer, t_model.outputs],
+							feed_dict={input:bptt_batch_x, label:bptt_batch_y, mask:bptt_batch_m,
+								t_model.initial_state[0]:np_state[0], t_model.initial_state[1]:np_state[1]})
 					print 'clipiter:', ci,', bptt index:', j, ':, loss:', bptt_batch_loss, ', accuracy:', acc*100., '%'
-					#print 'memory activations: ', 100. - ((np_state[0]<0).sum() + (np_state[1]<0).sum())/2., '%' #remove
-					#print 'bincount: ', np.bincount(out) # remove
-					#print p1 # remove
+					print 'memory activations: ', 100. - ((np_state[0]<0).sum() + (np_state[1]<0).sum())/128., '%' #remove
+					print 'out bincount:', np.bincount(np.array(out).flatten()) # remove
+					#print out
+					#print 'inp bincount:', np.bincount(np.array(bptt_batch_x.flatten()*3.75+7.5, dtype='uint8'))
+					#print p1.shape # remove
 					#print p2 # remove
 				save_path = saver.save(sess, "./params/last_model.ckpt")
 				print("Model saved in file: %s\n" % save_path)
--- src/model.py
+++ src/model.py
@@ -6,16 +6,16 @@ import tensorflow as tf
 	global_context_size: param for global context, default = 100
 	local_context_size: param for local context, default = 10
 	lstm_dim: dimension of the lstm layer for global context; default = 500
-	sampl_dim: dimension of down sampling layer; default = 90
-	hid_dim1: dimension of 1st hidden layer; default = 100
-	hid_dim2: dimension of 2nd hidden layer; default = 100
+	sampl_dim: dimension of down sampling layer; default = 10
+	hid_dim1: dimension of 1st hidden layer; default = 80
+	hid_dim2: dimension of 2nd hidden layer; default = 200
 	out_dim : dimension of softmax layer; default = 16
 	inputs is of shape: batch_size x (bptt_steps x global_context_size) x 1
 	labels is of shape: batch_size x (bptt_steps x global_context_size) x 16
 '''
 
 class sample_rnn():
-    def __init__(self, inputs, labels, bptt_steps=2, global_context_size=100, local_context_size=10, lstm_dim=100, sampl_dim=10,
+    def __init__(self, inputs, labels, masks, bptt_steps=2, global_context_size=100, local_context_size=10, lstm_dim=100, sampl_dim=10,
     	hid_dim1=80, hid_dim2=200, out_dim=16, batch_size=1, is_training=True):
 
 		self.weights = {
@@ -37,6 +37,7 @@ class sample_rnn():
 		self.mean_acc = 0.
 		self.outputs = []
 		self.loss = 0.
+		count = 0
 
 		with tf.variable_scope('RNN') as scope:
 			for i in range(bptt_steps):
@@ -47,9 +48,9 @@ class sample_rnn():
 				lstm_output, self.state = lstm_cell(global_context, self.state)
 				lstm_output = tf.nn.relu(lstm_output)
 				down_sampl = (tf.matmul(lstm_output, self.weights['sampl']) + self.biases['sampl'])
-				down_sampl = down_sampl/tf.reduce_sum(tf.abs(down_sampl), axis=1)
+				down_sampl = down_sampl/10.#tf.reduce_sum(tf.abs(down_sampl), axis=1)
 
-				if i==0:	self.o1, self.o2 = lstm_output, down_sampl # remove
+				#if i==0:	self.o1, self.o2 = lstm_output, down_sampl # remove
 				print i
 				for j in range(global_context_size):
 					pred_index = (i+1)*global_context_size + j
@@ -59,13 +60,18 @@ class sample_rnn():
 					hid1 = tf.nn.relu(tf.matmul(conc, self.weights['hidn1']) + self.biases['hidn1'])
 					hid2 = tf.nn.relu(tf.matmul(hid1, self.weights['hidn2']) + self.biases['hidn2'])
 					out = tf.matmul(hid2, self.weights['out']) + self.biases['out']
+					out = tf.multiply(out, masks[:, pred_index - global_context_size])
 					loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels[:, pred_index-global_context_size, :], logits=out))
-					correct_pred = tf.equal(tf.argmax(out, 1), tf.argmax(labels[:, pred_index-global_context_size, :], 1))
-					accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+					#if i==0 and j==0:	self.o1 = out
+					#loss = tf.multiply(loss, masks[:, pred_index-global_context_size])
+					correct_pred = tf.equal(tf.argmax(out, 1), tf.argmax(labels[:, pred_index - global_context_size, :], 1))
+					accuracy = tf.reduce_sum(tf.cast(correct_pred, tf.float32)) - (batch_size - tf.reduce_sum(masks[:, pred_index-global_context_size, 0]))
+					mean_acc = accuracy/tf.reduce_sum(masks[:, pred_index-global_context_size, 0])
 					self.loss += loss
-					self.mean_acc += accuracy
-					if is_training is False:	self.outputs.append(tf.argmax(out, 1)[0])
+					count+= tf.reduce_mean(masks[:, pred_index - global_context_size, 0])
+					self.mean_acc += mean_acc
+					if is_training is False:	self.outputs.append(tf.argmax(out, 1))
 
 			self.final_state = self.state
 			self.mean_acc /= (bptt_steps*global_context_size)
-			self.loss /= (bptt_steps*global_context_size)
+			self.loss /= count#(bptt_steps*global_context_size)
