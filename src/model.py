ac5020d9f9aaedc2fab7ee556fc498e060617e03
shady-cs15 <schakra1@andrew.cmu.edu>
2017-06-06 17:11:39 +0530

    first version of noise augmentation

--- src/model.py
+++ src/model.py
@@ -40,7 +40,7 @@ class sample_rnn():
 				'out':	tf.Variable(tf.zeros([out_dim]), name='out/b')
 				}
 
-		lstm_cell = tf.contrib.rnn.BasicLSTMCell(lstm_dim)#, activation=lu)
+		lstm_cell = tf.contrib.rnn.BasicLSTMCell(lstm_dim)
 		self.initial_state = self.state = lstm_cell.zero_state(batch_size, tf.float32)
 		self.total_loss = 0.
 		self.mean_acc = 0.
@@ -50,6 +50,10 @@ class sample_rnn():
 		self.generation_phase = tf.placeholder(tf.bool)
 		inputs = tf.cond(self.generation_phase, lambda:inputs[:,:global_context_size,:], lambda:inputs)
 
+		keep_prob = 0.3
+		global_mask = tf.concat([tf.ones(keep_prob*global_context_size), tf.zeros((1-keep_prob)*global_context_size)], 0)
+		local_mask = tf.concat([tf.ones(keep_prob*local_context_size), tf.zeros((1-keep_prob)*local_context_size)], 0)
+
 		print 'Building computation graph ..'
 		with tf.variable_scope('RNN') as scope:
 			for i in range(bptt_steps):
@@ -58,17 +62,24 @@ class sample_rnn():
 				global_context = inputs[:, i*global_context_size:(i+1)*global_context_size, :]
 				global_context = tf.reshape(global_context, [batch_size, global_context_size])
 				lstm_output, self.state = lstm_cell(global_context, self.state)
-				down_sampl = tf.nn.tanh(tf.matmul(lstm_output, self.weights['sampl']) + self.biases['sampl'])#/sampl_dim)
-				#down_sampl = down_sampl/tf.reduce_max(down_sampl)
-				#down_sampl = (down_sampl - tf.reduce_min(down_sampl))/(tf.reduce_max(down_sampl)-tf.reduce_min(down_sampl))
-				#down_sampl = tf.zeros([batch_size, 10])
+				down_sampl = tf.nn.tanh(tf.matmul(lstm_output, self.weights['sampl']) + self.biases['sampl'])
 
-				#if i==0:	self.o1, self.o2 = lstm_output, down_sampl  # remove
+				if i>0:
+					last_out = (tf.cast(tf.concat(self.outputs[-global_context_size:], 1), tf.float32) - 7.5)/7.5
+					global_mask = tf.random_shuffle(global_mask)
+					global_context = global_context*global_mask + last_out*(1-global_mask)
+
 				print 'Graph built:', i*100./bptt_steps, '%'
 				for j in range(global_context_size):
 					pred_index = (i+1)*global_context_size + j
 					local_context =  inputs[:, pred_index-local_context_size:pred_index, :]
 					local_context = tf.reshape(local_context, [batch_size, local_context_size])
+
+					if i>0:
+						last_local_out = (tf.cast(tf.concat(self.outputs[-local_context_size:], 1), tf.float32) - 7.5)/7.5
+						local_mask = tf.random_shuffle(local_mask)
+						local_context = local_context*local_mask + last_local_out*(1-local_mask)
+
 					conc = tf.concat([down_sampl, local_context], axis=1)
 					hid1 = tf.nn.relu(tf.matmul(conc, self.weights['hidn1']) + self.biases['hidn1'])
 					hid2 = tf.nn.relu(tf.matmul(hid1, self.weights['hidn2']) + self.biases['hidn2'])
@@ -83,9 +94,10 @@ class sample_rnn():
 					count += tf.reduce_mean(masks[:, pred_index - global_context_size, 0])
 
 					# sampling
+					sample = tf.multinomial(tf.log(tf.nn.softmax(out)), 1)
+					self.outputs.append(tf.reshape(sample, [batch_size, 1]))
+
 					if is_training is False:
-						sample = tf.multinomial(tf.log(tf.nn.softmax(out)), 1)
-						self.outputs.append(tf.argmax(out, 1))#sample)
 						last_pred = (tf.cast(sample, tf.float32) -7.5)/7.5
 						last_pred = tf.reshape(last_pred, [1, 1, 1])
 						inputs = tf.cond(self.generation_phase, lambda: tf.concat([inputs, last_pred], axis=1), lambda: inputs)
